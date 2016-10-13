1a949f9f5dee87997a486e407b4d9d91b8ec39c9
Durran Jordan <durran@gmail.com>
2016-10-12 20:07:28 -0400

    Bring in enzyme

--- src/renderer/component/polaris.jsx
+++ src/renderer/component/polaris.jsx
@@ -1,4 +1,5 @@
 const React = require('react');
+const PolarisStore = require('../store/polaris-store');
 
 /**
  * The polaris constant.
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -6,7 +6,12 @@ const Polaris = require('./component/polaris');
 const PolarisStore = require('./store/polaris-store');
 
 /**
+ * The root div the application is contained in.
+ */
+const APPLICATION = 'application';
+
+/**
  * Activate the package manager then render the polaris component.
  */
 PolarisStore.packageManager.activate();
-ReactDOM.render(React.createElement(Polaris), document.getElementById('application'));
+ReactDOM.render(React.createElement(Polaris), document.getElementById(APPLICATION));
