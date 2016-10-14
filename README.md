eb136503dae0b2efee902f304ac5701e48bd0aa8
Durran Jordan <durran@gmail.com>
2016-10-13 22:22:59 -0400

    Setting up style import

---
+++ README.md
@@ -0,0 +1,12 @@
+TODO:
+
+- Style the main application window.
+- Show home dashboard if exists.
+- Show dashboard list if dashboards exist.
+- Show visualisation list if visualisations exist.
+- Show zero state with shortcut to create visualisation.
+
+- Bundled visualisations:
+  - Schema anomalies
+  - Schema view
+  - Unused indexes
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,7 +1,9 @@
 require('babel-register')({ extensions: ['.jsx'], ignore: false });
 
+const path = require('path');
 const React = require('react');
 const ReactDOM = require('react-dom');
+const StyleManager = require('hadron-style-manager');
 const I18n = require('./i18n');
 const Polaris = require('./component/polaris');
 const PolarisStore = require('./store/polaris-store');
@@ -17,6 +19,16 @@ const APPLICATION = 'application';
 const EN_UK = 'en-uk';
 
 /**
+ * The directory to store the compiled less.
+ */
+const LESS_CACHE = path.join(__dirname, '.compiled-less');
+
+/**
+ * The root application stylesheet.
+ */
+const ROOT_STYLESHEET = path.join(__dirname, 'styles', 'index.less');
+
+/**
  * Initiate the i18n module, activate the package manager
  * and then render the polaris component.
  */
@@ -24,5 +36,6 @@ const i18n = new I18n();
 i18n.load(EN_UK, () => {
   global.t = i18n.get(EN_UK);
   PolarisStore.packageManager.activate();
+  new StyleManager(LESS_CACHE, __dirname).use(document, ROOT_STYLESHEET);
   ReactDOM.render(React.createElement(Polaris), document.getElementById(APPLICATION));
 });
